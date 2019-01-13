f368a815ac724029a4b756f133e7abb45448fa7b
Curtis <curtis@drumeo.com>
2019-01-12 20:44:42 -0800

    Removed old testing code. Now that I have a dedicated repo for controls/ui.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/basic-video.js',
     mode: 'development',
     resolve: {
         modules: ['node_modules']
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'basic-video.js'
     }
 };
